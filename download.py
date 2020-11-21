6c44e0ba0931e6b93c4ebc346e8ea35aaddb9e2b
Rodrigo507 <rodrigo.gutierrez@up.ac.pa>
2020-11-20 23:14:26 -0500

    Imagens

---
+++ download.py
@@ -0,0 +1,36 @@
+import wget
+import os, shutil
+def eliminarImagen():
+    folder = 'multimedia/imagenes'
+    for filename in os.listdir(folder):
+        file_path = os.path.join(folder, filename)
+        try:
+            if os.path.isfile(file_path) or os.path.islink(file_path):
+                os.unlink(file_path)
+            elif os.path.isdir(file_path):
+                shutil.rmtree(file_path)
+        except Exception as e:
+            print('Failed to delete %s. Reason: %s' % (file_path, e))
+    #os.remove("multimedia/imagenes/cambio.jpg")
+    #os.remove("multimedia/imagenes/img.png")
+def descargaImagen(url_img):
+    eliminarImagen()
+    wget.download(url_img,"multimedia/imagenes/img.png")
+
+def eliminarVideo():
+    folder = 'multimedia/videos'
+    for filename in os.listdir(folder):
+        file_path = os.path.join(folder, filename)
+        try:
+            if os.path.isfile(file_path) or os.path.islink(file_path):
+                os.unlink(file_path)
+            elif os.path.isdir(file_path):
+                shutil.rmtree(file_path)
+        except Exception as e:
+            print('Failed to delete %s. Reason: %s' % (file_path, e))
+    #os.remove("multimedia/imagenes/cambio.jpg")
+    #os.remove("multimedia/imagenes/img.png")
+def descargaVideo(url_video):
+    eliminarVideo()
+    wget.download(url_video,"multimedia/videos/video.mp4")
+
